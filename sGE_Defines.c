c73e0ccd09623ce16f45cd7cdd18545ac5f3add3
s0lly <39304402+s0lly@users.noreply.github.com>
2021-08-18 22:33:39 +0100

    Update sGE_Defines.c
    Functions are generated per struct to allow for stronger type checking

--- sGE_Defines.c
+++ sGE_Defines.c
@@ -22,70 +22,100 @@ typedef double f64;
 
 typedef int32_t b32;
 
-static i32 _Array_GetElementIndexFromIDCalc(void *data, i32 count, i32 i)
-{
-	for (i32 index = 0; index < count; index++)
-	{
-		if (*((i32 *)data + index) == i)
-			return index;
-	}
-	return -1;
-}
-
-
-static void *_Array_GetElementFromIDCalc(void *data, i32 count, i32 i, u64 elementArrayOffsetBytes, u64 elementSize)
-{
-	i32 index = _Array_GetElementIndexFromIDCalc(data, count, i);
-
-	if (index < 0)
-	{
-		return 0;
-	}
-	else
-	{
-		return (void *)((u8 *)data + elementArrayOffsetBytes + elementSize * index);
-	}
-}
-
-
-static void *_Array_PushDataCalc(void *data, void *dataToCopy, u64 elementArrayOffsetBytes, u64 elementSize, u64 idArrayOffsetBytes, u64 idSize, u64 nextIDOffsetBytes, u64 countOffsetBytes)
-{
-	i32 count = *(i32 *)((u8 *)data + countOffsetBytes);
-	memcpy(((u8 *)data + elementArrayOffsetBytes + elementSize * count), dataToCopy, elementSize);
-	*(u32 *)((u8 *)data + idArrayOffsetBytes + idSize * count) = *(i32 *)((u8 *)data + nextIDOffsetBytes);
-	(*(u32 *)((u8 *)data + nextIDOffsetBytes))++;
-	(*(u32 *)((u8 *)data + countOffsetBytes))++;
-
-	return (void *)((u8 *)data + elementArrayOffsetBytes + elementSize * count);
-}
-
-
-#define GenerateStructWithGenerics(prefix, type, n, struc)\
-typedef struct prefix##type##ID\
-{\
-	i32 val;\
-} prefix##type##ID;\
-typedef struct type struc type;\
-typedef struct prefix##type##Array\
-{\
-	prefix##type##ID ids[n];\
-	prefix##type##ID nextID;\
-	type e[n];\
-	i32 count;\
-} prefix##type##Array;
-
-
 
 // TODO: Below functions require bounds checking etc, possibly via helper functions
-#define Array_GetCountMax(arr) (sizeof((arr)->e) / sizeof(((arr)->e)[0]))
-#define Array_GetElement(arr, n) (&(arr)->e[n])
-#define Array_GetElementAtCount(arr) (&(arr)->e[(arr)->count])
-#define Array_PushData(arr, data) _Array_PushDataCalc((arr), &data, (u8 *)((arr)->e) - (u8 *)((arr)), sizeof(((arr)->e)[0]), (u8 *)((arr)->ids) - (u8 *)((arr)), sizeof(((arr)->ids)[0]), (u8 *)(&(arr)->nextID) - (u8 *)((arr)), (u8 *)(&(arr)->count) - (u8 *)((arr)))
-#define Array_PushZeroed(arr) { memset(&(arr)->e[(arr).count], 0, sizeof((arr)->e[(arr).count])); (arr)->count++; }
-#define Array_ReplaceIndex(arr, index, data) { (arr)->e[(index)] = (data); }
-#define Array_ResetIndex(arr, index) { memset(&(arr)->e[(index)], 0, sizeof((arr)->e[(index)])); }
-#define Array_Pop(arr) { if ((arr).count > 0 ) (arr)->count--; }
-#define Array_GetElementFromID(arr, i) _Array_GetElementFromIDCalc((arr), (arr)->count, (i).val, (u8 *)((arr)->e) - (u8 *)((arr)),  sizeof(((arr)->e)[0]))
-#define Array_GetID(arr, n) ((arr)->ids[n])
-#define Array_GetIDFromElement(arr, elem) ((arr)->ids[((u8 *)(elem) - (u8 *)(arr)->e) / sizeof(((arr)->e)[0])])
-#define Array_GetIndexFromElement(arr, elem) (((u8 *)(elem) - (u8 *)(arr)->e) / sizeof(((arr)->e)[0]))
+// NOTE: IDs always start at 1, this allows us to identify unitialized IDs via a 0 result
+#define GenerateStructWithGenerics(type, n, struc)\
+typedef struct type##ID\
+{\
+	i32 val;\
+} type##ID;\
+typedef struct type struc type;\
+typedef struct type##Array\
+{\
+	type##ID ids[n];\
+	type##ID nextID;\
+	type e[n];\
+	i32 count;\
+} type##Array;\
+static i32 type##Array_GetCountMax(type##Array *arr)\
+{\
+	return (i32)(sizeof((arr)->e) / sizeof(((arr)->e)[0]));\
+}\
+static type * type##Array_GetElement(type##Array *arr, i32 index)\
+{\
+	return (&(arr)->e[index]);\
+}\
+static type * type##Array_GetElementAtCount(type##Array *arr)\
+{\
+	return (&(arr)->e[(arr)->count]);\
+}\
+static type * type##Array_PushData(type##Array *arr, type data)\
+{\
+	assert((arr)->count < type##Array_GetCountMax(arr));\
+	(arr)->e[(arr)->count] = data;\
+	(arr)->ids[(arr)->count] = (type##ID) { ((arr)->nextID.val + 1) };\
+	(arr)->count++;\
+	(arr)->nextID.val++;\
+	return (&(arr)->e[(arr)->count - 1]);\
+}\
+static type * type##Array_PushZeroed(type##Array *arr)\
+{\
+	return type##Array_PushData(arr, (type) { 0 });\
+}\
+static void type##Array_ReplaceIndex(type##Array *arr, i32 index, type data)\
+{\
+	assert(index >= 0 && index < (arr)->count);\
+	(arr)->e[index] = data;\
+}\
+static void type##Array_ResetIndex(type##Array *arr, i32 index)\
+{\
+	type##Array_ReplaceIndex(arr, index, (type) { 0 });\
+	(arr)->ids[index].val = 0;\
+}\
+static type type##Array_Pop(type##Array *arr)\
+{\
+	type result = (arr)->e[(arr)->count - 1];\
+	type##Array_ResetIndex(arr, (arr)->count - 1);\
+	return result;\
+}\
+static i32 _##type##Array_GetIndexFromID(type##Array *arr, type##ID IDToCheck)\
+{\
+	for (i32 indexCounter = 0; indexCounter < (arr)->count; indexCounter++)\
+	{\
+		if ((arr)->ids[indexCounter].val == IDToCheck.val)\
+			return indexCounter;\
+	}\
+	return -1;\
+}\
+static type * type##Array_GetElementFromID(type##Array *arr, type##ID IDToCheck)\
+{\
+	i32 index = _##type##Array_GetIndexFromID(arr, IDToCheck);\
+	if (index == -1)\
+	{\
+		return 0;\
+	}\
+	return &((arr)->e[index]);\
+}\
+static type##ID type##Array_GetID(type##Array *arr, i32 index)\
+{\
+	return (arr)->ids[index];\
+}\
+static i32 type##Array_GetIndexFromElement(type##Array *arr, type *elem)\
+{\
+	return (i32)(elem - &(arr)->e[0]);\
+}\
+static type##ID type##Array_GetIDFromElement(type##Array *arr, type *elem)\
+{\
+	i32 index = type##Array_GetIndexFromElement(arr, elem);\
+	return type##Array_GetID(arr, index);\
+}\
+static void type##Array_Swap(type##Array *arr, i32 indexFirst, i32 indexSecond)\
+{\
+	type tempType = (arr)->e[indexFirst];\
+	type##ID tempID = (arr)->ids[indexFirst];\
+	(arr)->e[indexFirst] = (arr)->e[indexSecond];\
+	(arr)->ids[indexFirst] = (arr)->ids[indexSecond];\
+	(arr)->e[indexSecond] = tempType;\
+	(arr)->ids[indexSecond] = tempID;\
+}
